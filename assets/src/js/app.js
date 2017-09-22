4008042875895ffd913bed7484e54a8662ad76cf
Amy Dutton <amy@ahhacreative.com>
2017-09-22 04:38:54 -0500

    Styled form fields

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,3 +1,7 @@
+//@prepros-prepend vendor/jquery-3.2.1.js
+//@prepros-prepend vendor/modernizr.js
+//@prepros-prepend vendor/jquery.fitvids.js
+
 jQuery ( function($) {
 
 	var site = new SiteController($);
@@ -10,6 +14,8 @@ function SiteController($)
 	self.init = function()
 	{
 		initMobileNav();
+		initHandleLabels();
+		initHandleFitVid();
 	}
 
 	function initMobileNav() {
@@ -25,6 +31,28 @@ function SiteController($)
 			e.preventDefault();
 			$('.js-mobile-nav').removeClass('open');
 		});
+	}
+
+	function initHandleLabels() {
+		$('input[type=text]').focus(function(e) {
+			$(this).parent().find('label').addClass('out-of-the-way');
+		});
+
+		$('input[type=text]').blur(function(e) {
+
+			// if the field has a value, leave it
+			if ($(this).val() || $(this).val() != '') {
+				return;
+			}
+
+			// otherwise, move the label back
+			$(this).parent().find('label').removeClass('out-of-the-way');
+
+		});
+	}
+
+	function initHandleFitVid() {
+		 $(".js-fitvid").fitVids();
 	}
 
 	return self;
